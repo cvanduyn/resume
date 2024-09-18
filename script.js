2ba185cd87da4d1bd8f33f9cc86472bfba0b3481
cvanduyn <cory.vanduyn@gmail.com>
2024-09-18 12:02:20 -0400

    removed keen-slider, will try to figure that out at a later date for the testimonial carousel

--- script.js
+++ script.js
@@ -23,22 +23,4 @@ function scrollToTop() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const slider = new KeenSlider("#all_testimonials", {
-    loop: true,                // Loop through slides
-    centered: true,            // Centers the active slide
-    slidesPerView: 1.5,        // Show part of the next slide
-    spacing: 20,               // Space between slides
-    breakpoints: {
-      "(min-width: 600px)": {
-        slidesPerView: 2.5,    // Show 2.5 slides on medium screens
-        spacing: 20,           // Adjust space between slides
-      },
-      "(min-width: 1000px)": {
-        slidesPerView: 3.5,    // Show 3.5 slides on large screens
-        spacing: 30,           // More space between slides on large screens
-      },
-    },
-  });
-});
 
